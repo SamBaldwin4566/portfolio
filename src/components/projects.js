417d04ec40feef14a3c63ced60f10759c81b025d
Sam Baldwin <sjbaldwin93@icloud.com>
2023-06-13 13:19:34 +0100

    added library project link

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,8 +1,20 @@
+import library from '../images/Library.png'
+
 function Projects() {
 
     return(
         <div id="projects">
-            <div className="pageHeading"><b>Projects</b></div>
+            <div className="pageHeading"><b>Projects</b>
+                <div id="projectsGrid">
+                    {/* <div class="projects" id="library">
+                        Library
+                    </div> */}
+                    <div class="projectsInfo" id="libraryInfo">
+                    <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectPic" src={library} alt="Screenshot"></img></a>
+                        
+                    </div>
+                </div>    
+            </div>
         </div>
     )
 }
