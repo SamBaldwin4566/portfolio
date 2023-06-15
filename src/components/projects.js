bfef43e549c153cf96f6894e869f1cda75f8080b
Sam Baldwin <sjbaldwin93@icloud.com>
2023-06-15 13:48:49 +0100

    responsive design for all pages

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,15 +5,28 @@ function Projects() {
     return(
         <div id="projects">
             <div className="pageHeading"><b>Projects</b>
-                <div id="projectsGrid">
-                    {/* <div class="projects" id="library">
-                        Library
-                    </div> */}
-                    <div class="projectsInfo" id="libraryInfo">
-                    <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectPic" src={library} alt="Screenshot"></img></a>
-                        
-                    </div>
-                </div>    
+                <table id="projectsGrid">
+                    <tr>
+                        <td>
+                            <div class="projectsInfo" id="libraryInfo">
+                            <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectPic" src={library} alt="Screenshot"></img></a>
+                            
+                            </div>
+                        </td>
+                        <td>
+                            <div class="projectsInfo" id="libraryInfo">
+                            <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectPic" src={library} alt="Screenshot"></img></a>
+                            
+                            </div>
+                        </td>
+                        <td>
+                            <div class="projectsInfo" id="libraryInfo">
+                            <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectPic" src={library} alt="Screenshot"></img></a>
+                            
+                            </div>
+                        </td>
+                    </tr>
+                </table>    
             </div>
         </div>
     )
