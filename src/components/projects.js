aa3838536431246ca94795a3e026b69cd3154be7
Sam Baldwin <sjbaldwin93@icloud.com>
2023-11-16 13:18:47 +0000

    added new project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,6 @@
 import library from '../images/LibraryApp.png'
-import cloneWebpage from '../images/CloneWebpage.png'
 import gamesApp from '../images/GamesApp.png'
+import wedding from '../images/WeddingWebsite.png'
 
 function Projects() {
 
@@ -13,16 +13,19 @@ function Projects() {
             <h1 className="name"><span className="firstLetter"><b>P</b></span>rojects</h1>
 
 
-                <div id="projectsContainer">
+                <div class="projectsContainer">
+                    <div id="wedding">
+                        <a href="http://sbaldwin-wedding.netlify.app" target="_blank" rel="noreferrer"><img class="projectsImg" src={wedding} alt=""></img></a>
+                        <h1>Wedding Website</h1>
+                    </div>
                     <div id="library">
-                        <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectsImg" src={library}></img></a>
+                        <a href="http://sbaldwin-library.netlify.app" target="_blank" rel="noreferrer"><img class="projectsImg" src={library} alt=""></img></a>
                         <h1>Library App</h1>
                     </div>
                     <div id="gamesApp">
-                        <a href="http://sbaldwin-games-app.netlify.app" target="_blank" rel="noreferrer"><img class="projectsImg" src={gamesApp}></img></a>
+                        <a href="http://sbaldwin-games-app.netlify.app" target="_blank" rel="noreferrer"><img class="projectsImg" src={gamesApp} alt=""></img></a>
                         <h1>Games App</h1>
                     </div>
-                    
                     
                 </div>
                 
